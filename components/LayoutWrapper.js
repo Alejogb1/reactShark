3bdead602818f9a4aea95bea15eb0f900864f853
Alejogb1 <52103963+Alejogb1@users.noreply.github.com>
2021-05-19 16:11:08 -0300

    Update LayoutWrapper.js

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -13,17 +13,13 @@ const LayoutWrapper = ({ children }) => {
       <div className="flex flex-col justify-between h-screen">
         <header className="flex items-center justify-between py-10">
           <div>
-            <Link href="/" aria-label="Tailwind CSS Blog">
+            <Link href="/" aria-label="ReactShark">
               <div className="flex items-center justify-between">
                 <div className="mr-3">
                   <Logo />
                 </div>
-                {typeof siteMetadata.headerTitle === 'string' ? (
-                  <div className="hidden h-6 text-2xl font-semibold sm:block">
-                    {siteMetadata.headerTitle}
-                  </div>
-                ) : (
-                  siteMetadata.headerTitle
+                {typeof siteMetadata.headerTitle === 'string' && (
+                  <div className="hidden h-6 text-2xl font-semibold sm:block">ReactShark</div>
                 )}
               </div>
             </Link>
